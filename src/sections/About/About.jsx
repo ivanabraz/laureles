1c57f2e50963fbe5282699b7a84a09080bb7f2c1
Ivana Braz <braz.ivana@gmail.com>
2024-11-08 21:41:38 -0300

    Adding swiper to songs

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const About = ({ t }) => {
     const image = `${process.env.PUBLIC_URL}/images/photos/image-01.jpg`;
     return (
         <div className="w-full grid grid-cols-2 xs:grid-cols-1 md:grid-cols-2">
             <div className="w-2/4 xs:w-3/4 sm:w-3/4 md:w-3/4 m-auto py-16 text-base lg:text-lg condensed text-justify">
-                {t('global.about-text')}
+                <motion.p
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true }}
+                    transition={{ duration: 1, ease: "easeInOut" }}
+                >
+                    {t('global.about-text')}
+                </motion.p>
             </div>
-            <img 
+            <motion.img
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 1, ease: "easeInOut" }}
                 src={image} 
                 alt={'Cuero'} 
                 className="aspect-square p-10 w-full h-full object-cover -z-10" 
--- src/sections/Songs/Songs.jsx
+++ src/sections/Songs/Songs.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
 
 const Songs = ({ t }) => {
-    const image = `${process.env.PUBLIC_URL}/images/songs/toro.png`;
-    const [currentSong, setCurrentSong] = useState(null); // Almacena la canción actual
-    const [isPlaying, setIsPlaying] = useState(false); // Controla el estado de reproducción
-    const [audio, setAudio] = useState(null); // Referencia al audio actual
-    const [isPaused, setIsPaused] = useState(false); // Controla si la animación está pausada
+    const [currentSong, setCurrentSong] = useState(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [audio, setAudio] = useState(null);
+    const [isPaused, setIsPaused] = useState(false);
 
     const music = [
         { name: 'Toro', id: 'toro' },
@@ -15,8 +17,6 @@ const Songs = ({ t }) => {
         { name: 'La Oriental', id: 'la-oriental' },
         { name: 'Paladar Negro', id: 'paladar-negro' },
     ];
-
-    // Efecto para pausar el audio cuando se cambia de canción o se desmonta el componente
     useEffect(() => {
         return () => {
             if (audio) {
@@ -25,26 +25,22 @@ const Songs = ({ t }) => {
         };
     }, [audio]);
 
-    // Función para manejar la reproducción de canciones
     const handlePlayPause = (song) => {
         if (currentSong?.id === song.id && isPlaying) {
-            // Si se está reproduciendo la canción actual, pausarla
             audio.pause();
             setIsPlaying(false);
-            setIsPaused(true); // Pausar la animación
+            setIsPaused(true);
         } else {
-            // Si es una canción nueva, reproducirla
             if (audio) {
-                audio.pause(); // Pausar la canción anterior
+                audio.pause();
             }
             const newAudio = new Audio(`${process.env.PUBLIC_URL}/images/songs/${song.id}.mp3`);
             newAudio.play();
             setAudio(newAudio);
             setCurrentSong(song);
             setIsPlaying(true);
-            setIsPaused(false); // Reanudar la animación
+            setIsPaused(false);
 
-            // Cuando termina la canción, resetear el estado
             newAudio.onended = () => {
                 setIsPlaying(false);
             };
@@ -54,40 +50,52 @@ const Songs = ({ t }) => {
     return (
         <div className="w-full bg-black text-center py-16 condensed uppercase text-white">
             <p className="text-4xl pb-14">{t('global.songs')}</p>
-            <div className="px-20 w-full grid grid-cols-4 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-                {music.map((song) => (
-                    <div
-                        key={song.id}
-                        className="py-5 cursor-pointer"
-                        onClick={() => handlePlayPause(song)}
-                    >
-                        <img
-                            src={image}
-                            alt={song.name}
-                            className={`w-full h-auto transition-transform ${
-                                currentSong?.id === song.id
-                                    ? isPlaying
-                                        ? 'animate-spin-slow'
-                                        : 'animate-spin-slow paused'
-                                    : ''
-                            }`}
-                            style={{
-                                animationPlayState:
-                                    currentSong?.id === song.id && isPaused ? 'paused' : 'running',
-                            }}
-                        />
-                        <FontAwesomeIcon
-                            icon={currentSong?.id === song.id && isPlaying ? faPause : faPlay}
-                            size="md"
-                            className={`mt-5 text-white transition-opacity ${
-                                currentSong?.id === song.id ? 'opacity-100' : 'opacity-0'
-                            }`}
-                        />
-                        <p className="text-lg text-white">{song.name}</p>
-                        <p className="text-xs text-white">Cuero</p>
-                    </div>
-                ))}
-            </div>
+            <motion.div initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 3, ease: "easeInOut" }}
+            viewport={{ once: true }}
+            >
+                <Swiper
+                    spaceBetween={20}
+                    breakpoints={{
+                        320: { slidesPerView: 1.5 },
+                        640: { slidesPerView: 1.5 },
+                        768: { slidesPerView: 3.5 },
+                        1024: { slidesPerView: 3.5 },
+                    }}
+                    grabCursor={true}
+                >
+                    {music.map((song) => (
+                        <SwiperSlide key={song.id} className="flex flex-col items-center" onClick={() => handlePlayPause(song)}>
+                            <img
+                                src={`${process.env.PUBLIC_URL}/images/songs/${song.id}.png`}
+                                alt={song.name}
+                                className={` w-full h-auto lg:px-10 cursor-pointer transition-transform ${
+                                    currentSong?.id === song.id
+                                        ? isPlaying
+                                            ? 'animate-spin-slow'
+                                            : 'animate-spin-slow paused'
+                                        : ''
+                                }`}
+                                style={{
+                                    animationPlayState:
+                                        currentSong?.id === song.id && isPaused ? 'paused' : 'running',
+                                }}
+                            />
+                            <FontAwesomeIcon
+                                icon={currentSong?.id === song.id && isPlaying ? faPause : faPlay}
+                                size="md"
+                                className={`mt-5 text-white transition-opacity cursor-pointer ${
+                                    currentSong?.id === song.id ? 'opacity-100' : 'opacity-0'
+                                }`}
+                            />
+                            <p className="text-lg text-white cursor-pointer" onClick={() => handlePlayPause(song)}>{song.name}</p>
+                            <p className="text-xs text-white" onClick={() => handlePlayPause(song)}>Cuero</p>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </motion.div>
+
         </div>
     );
 };
