39b1537271e29704792bcc333f5b8cdc3ccdd1a9
Ivana Braz <braz.ivana@gmail.com>
2024-11-09 22:44:37 -0300

    Fixing deploy

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -5,15 +5,20 @@ const Contact = ({ t }) => {
     return (
     <div id='contact' className="w-full bg-black py-16 text-center condensed uppercase text-white flex flex-col justify-center">
             {/* Título */}
-            <p className="text-4xl mb-14">
-                {t('global.contact')}
-            </p>
-
+            <motion.p
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true }}
+                    transition={{ duration: 1, ease: "easeInOut" }}
+                    className="text-4xl mb-14"
+                >
+                    {t('global.contact')}
+                </motion.p>
             <motion.div
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true }}
-                transition={{ duration: 1, ease: "easeInOut" }}
+                transition={{ duration: 2, ease: "easeInOut" }}
                 className="w-[60%] grid grid-cols-1 md:grid-cols-3 m-auto"
             >
                 <a href="mailto:cuerotango@gmail.com" rel="noopener noreferrer" target="_blank">cuerotango@gmail.com</a>
