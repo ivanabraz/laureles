866625e4a79afec14b207de1efb07ac39ad6b811
Ivana Braz <braz.ivana@gmail.com>
2025-06-29 00:07:57 -0300

    Fixing details, and CEO

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -20,7 +20,7 @@ const NavBar = ({ onLanguageChange, currentLanguage }) => {
             { name: 'Instagram', icon: faInstagram, href: 'instagram.com/laurelesaudiovisual' },
             { name: 'Whatsapp', icon: faWhatsapp, href: 'api.whatsapp.com/send?phone=5491133752356&text=%20Hola,%20%c2%bfpuedes%20brindarme%20informaci%c3%b3n%20sobre%20las%20sesiones%20de%20fotograf%c3%ada%20en%20Laureles%20Audiovisual%3F%20%f0%9f%93%b8%f0%9f%90%be%20Estoy%20interesado/a%20en%20capturar%20momentos%20especiales.%20%c2%a1Gracias!&fbclid=PAZXh0bgNhZW0CMTEAAad6ZrVzURR5nBa3pjyZXMVmdZ0ZWhXG0Fq_s9bjzZKkjnb8cofqbX4WjCUNxg_aem_tM7HFUsxjO-Jt-EWBDDbVA' },
             { name: 'TikTok', icon: faTiktok, href: 'tiktok.com/@laureles.audiovisual' },
-            { name: 'Pinterest', icon: faPinterest, href: 'o.pinterest.com/laurelesaudiovisual' },
+            { name: 'Pinterest', icon: faPinterest, href: 'pinterest.com/laurelesaudiovisual' },
         ]
     };
 
